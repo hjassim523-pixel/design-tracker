a93da0ffc4830aec6204f7adcff1c8040821deea
hasan <hjassim523@gmail.com>
2026-08-22 16:48:43 +0300

    new print

--- analyze.py
+++ analyze.py
@@ -8,3 +8,5 @@ completed_projects = df[df['is_completed'] == False]
 sorted_df = completed_projects.sort_values(by='price', ascending=False)
 print(total_price)
 print(completed_projects)
+print(sorted_df)
+ 
