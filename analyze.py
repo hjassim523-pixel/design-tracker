0dfd782192b46cd6158e00cfecad9ec176c87291
hasan <hjassim523@gmail.com>
2026-08-22 16:45:38 +0300

    first project

---
+++ analyze.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+columns = ["client_name", "project_type", "price", "is_completed"]
+df = pd.read_csv("C:/Users/LOQ/DesignTracker/projects.txt", names=columns)
+print(df)
+total_price = df['price'].sum()
+completed_projects = df[df['is_completed'] == False]
+sorted_df = completed_projects.sort_values(by='price', ascending=False)
+print(total_price)
+print(completed_projects)
